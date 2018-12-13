dcb08519141553b85c8a5b56cb9311a036f58ca1
Curtis Parham <parhamcurtis@gmail.com>
2018-12-12 18:56:37 -0600

    initial commit

---
+++ core/Input.php
@@ -0,0 +1,12 @@
+<?php
+
+class Input {
+
+  public static function get($input) {
+    if(isset($_POST[$input])) {
+      return FH::sanitize($_POST[$input]);
+    } else if(isset($_GET[$input])) {
+      return FH::sanitize($_GET[$input]);
+    }
+  }
+}
