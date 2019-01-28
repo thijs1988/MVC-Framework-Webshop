f4eb0f4ddba4eac619ca3acebed5a23dbcb4558b
Curtis Parham (FREESKILLS) <parhamcurtis@gmail.com>
2019-01-28 20:40:49 +0000

    H.php edited online with Bitbucket

--- core/H.php
+++ core/H.php
@@ -13,7 +13,7 @@ class H {
 
   public static function currentPage() {
     $currentPage = $_SERVER['REQUEST_URI'];
-    if($currentPage == PROOT || $currentPage == PROOT. DEFAULT_CONTROLLER .'/index') {
+    if($currentPage == PROOT || $currentPage == PROOT. strtolower(DEFAULT_CONTROLLER) .'/index') {
       $currentPage = PROOT . strtolower(DEFAULT_CONTROLLER);
     }
     return $currentPage;
