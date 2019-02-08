2092b9ea74eb4d313083771f484ddf84d503f949
Curtis Parham <parhamcurtis@gmail.com>
2019-02-08 09:05:07 -0600

    beginning work on database migrations

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,10 +3,13 @@
   use Core\Controller;
   use App\Models\Users;
   use Core\H;
+  use Core\Migration;
 
   class HomeController extends Controller {
 
     public function indexAction() {
+      $mig = new Migration();
+      $mig->createTable("test");
       $this->view->render('home/index');
     }
   }
---
+++ core/Migration.php
@@ -0,0 +1,30 @@
+<?php
+namespace Core;
+use Core\DB;
+
+class Migration extends DB{
+  protected $_db;
+
+  protected $_columnTypesMap = [
+    'int' => 'INT', 'INT' => 'INT', 'integer' => 'INT', 'tinyint' => 'TINYINT', 'smallint' => 'SMALLINT',
+    'mediumint' => 'MEDIUMINT', 'bigint' => 'BIGINT', 'numeric' => 'DECIMAL()', 'decimal' => 'DECIMAL()',
+    'double' => 'DOUBLE()', 'float' => 'FLOAT()', 'bit' => 'BIT()',
+  ];
+
+  public function __construct(){
+    $this->_db = DB::getInstance();
+  }
+
+  public function createTable($table){
+    $sql = "CREATE TABLE IF NOT EXISTS {$table} (
+      id INT AUTO_INCREMENT,
+      PRIMARY KEY (id)
+      )  ENGINE=INNODB;";
+
+    $this->_db->query($sql);
+  }
+
+  public function addColumn($table, $name, $type, $attrs=[]){
+    $sql = "ALTER TABLE {$table} ADD COLUMN {$name} VARCHAR(15) AFTER name;";
+  }
+}
