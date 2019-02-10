901f86bc1cadd04ec2a6c8160dae9e499bec5248
Curtis Parham <parhamcurtis@gmail.com>
2019-02-10 09:29:47 -0600

    fixed config

--- config/config.php
+++ config/config.php
@@ -2,15 +2,15 @@
 
   define('DEBUG', true); // set debug to false for production
 
-  define('DB_NAME', 'nginxruah'); // database name
+  define('DB_NAME', 'ruah'); // database name
   define('DB_USER', 'root'); // database user
-  define('DB_PASSWORD', 'root'); // database password
+  define('DB_PASSWORD', ''); // database password
   define('DB_HOST', '127.0.0.1'); // database host *** use IP address to avoid DNS lookup
 
   define('DEFAULT_CONTROLLER', 'Home'); // default controller if there isn't one defined in the url
   define('DEFAULT_LAYOUT', 'default'); // if no layout is set in the controller use this layout.
 
-  define('PROOT', '/'); // set this to '/' for a live server.
+  define('PROOT', '/ruah/'); // set this to '/' for a live server.
   define('VERSION','0.25'); // release version this can be used to display version or version assets like css and js files useful for fighting cached browser files
 
   define('SITE_TITLE', 'Ruah MVC Framework'); // This will be used if no site title is set
